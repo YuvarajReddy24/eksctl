a9eebfb48e42c4d333773cbe27932b902985ccfa
yuvaraj <yuvarajreddy24@gmail.com>
2025-11-29 23:24:07 +0530

    .

--- workstation.sh
+++ workstation.sh
@@ -11,6 +11,7 @@ usermod -aG docker ec2-user
 
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.34.2/2025-11-13/bin/linux/amd64/kubectl
 chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
 kubectl version --client
 
 ==eksctl===
